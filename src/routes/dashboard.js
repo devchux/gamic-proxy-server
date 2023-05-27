14e661048e213a2880d68ac87a8a200cb6196ece
Chukwudi Eze <chukwudieze97@gmail.com>
2023-05-27 12:35:37 +0100

    prettier formatter

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -3,10 +3,19 @@ const { requestMiddleware } = require("../utils/helper");
 
 const router = express.Router();
 
-router.get("/summary", requestMiddleware("https://gamic.app/api/dashboard/summary"));
+router.get(
+  "/summary",
+  requestMiddleware("https://gamic.app/api/dashboard/summary")
+);
 
-router.get("/summary2", requestMiddleware("https://gamic.app/api/dashboard/summary2"));
+router.get(
+  "/summary2",
+  requestMiddleware("https://gamic.app/api/dashboard/summary2")
+);
 
-router.get("/guilds", requestMiddleware("https://gamic.app/api/dashboard/guilds"));
+router.get(
+  "/guilds",
+  requestMiddleware("https://gamic.app/api/dashboard/guilds")
+);
 
 module.exports = router;
